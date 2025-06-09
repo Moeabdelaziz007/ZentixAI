3dc3c36d831a7cbe123545b9d19b35a51a2cc930
Mohamed Abdelaziz ‬‏ <amrikyy@gmail.com>
2025-06-09 17:55:34 +0300

    Add basic test suite

---
+++ tests/test_calculator_plugin.py
@@ -0,0 +1,12 @@
+import unittest
+
+from plugin_example import CalculatorPlugin
+
+class TestCalculatorPlugin(unittest.TestCase):
+    def test_addition(self):
+        plugin = CalculatorPlugin()
+        result = plugin.execute({'a': 5, 'b': 3})
+        self.assertEqual(result, {'result': 8})
+
+if __name__ == '__main__':
+    unittest.main()
